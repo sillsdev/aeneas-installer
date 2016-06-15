00daf9940321b401863d6dc2a2be969e106a99a3
Daniel Bair <daniel@danielbair.com>
2016-06-15 14:00:31 +0700

    Update build_packages.sh
    Moved brew installs to build_setup.sh

--- build_packages.sh
+++ build_packages.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-brew tap timsutton/formulae
-brew install brew-pkg
-brew install ruby
-brew link ruby
-gem install fpm
-
 #brew pkg --with-deps --scripts python #fails to build a working package
-zip -v python-2.7.11-macosx10.6.pkg.zip python-2.7.11-macosx10.6.pkg
 brew pkg --with-deps ffmpeg
 brew pkg --with-deps --scripts espeak_install_scripts espeak
 fpm -f --verbose --osxpkg-identifier-prefix org.python --python-pip=/usr/local/bin/pip -s python -t osxpkg BeautifulSoup4
@@ -20,15 +13,13 @@ if [ ! -f "/usr/local/bin/packagesbuild" ]; then
 	curl -fsSL -O http://s.sudre.free.fr/Software/files/Packages.dmg
 	#open Packages.dmg
 	hdiutil attach Packages.dmg
-	sudo installer -target / -pkg /Volumes/Packages\ 1.1.3/packages/Packages.pkg
-	hdiutil eject /Volumes/Packages\ 1.1.3/
+	sudo installer -target / -pkg /Volumes/Packages\ */packages/Packages.pkg
+	hdiutil eject /Volumes/Packages\ */
 fi
 
-brew install markdown
+zip -v python-2.7.11-macosx10.6.pkg.zip python-2.7.11-macosx10.6.pkg
 curl -fsSL https://raw.githubusercontent.com/readbeyond/aeneas/master/README.md | markdown > Aeneas_Readme.html
 
-mkdir ../../build
-#open Aeneas_Installer.pkgproj
+mkdir -p ../../build
 packagesbuild -v Aeneas_Installer.pkgproj
 open -R ../../build/Aeneas_Installer.mpkg
-
