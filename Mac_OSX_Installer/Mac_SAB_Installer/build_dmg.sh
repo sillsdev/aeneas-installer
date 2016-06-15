a087ad7439308743f53a3a89e3f68d8bd6914385
Daniel Bair <bair.daniel@gmail.com>
2016-06-16 06:41:52 +0700

    Update files.

--- Mac_OSX_Installer/Mac_SAB_Installer/build_dmg.sh
+++ Mac_OSX_Installer/Mac_SAB_Installer/build_dmg.sh
@@ -6,6 +6,11 @@ fi
 if [ ! -f "/usr/local/bin/appdmg" ]; then
 	npm install -g appdmg
 fi
+if [ ! -f "/Applications/Scripture App Builder.app" ]; then
+	open "https://drive.google.com/uc?export=download&id=0BzKJe4QoGwmXZHFQOXNTT2w4eHc"
+	echo "Cannot find /Applications/Scripture App Builder.app"
+	exit
+fi
 
 rm -vf ./Scripture\ App\ Builder-1.11.1.dmg
 appdmg appdmg.json ./Scripture\ App\ Builder-1.11.1.dmg
