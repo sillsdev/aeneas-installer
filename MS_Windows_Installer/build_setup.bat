b00b4e1ab3d94049c79fcf66258abf31d404faf7
Daniel Bair <bair.daniel@gmail.com>
2016-06-16 17:26:57 +0700

    Update wget.vbs script

--- MS_Windows_Installer/build_setup.bat
+++ MS_Windows_Installer/build_setup.bat
@@ -1,22 +1,22 @@
 @echo off
 
 echo Downloading 7-zip... 
-cscript.exe /B wget.vbs "http://www.7-zip.org/a/7z1602.exe" "%cd%\7z1602.exe"
+cscript.exe /NoLogo wget.vbs "http://www.7-zip.org/a/7z1602.exe" "%cd%\7z1602.exe"
 echo Installing 7-zip...
 7z1602.exe
 
 echo Downloading Inno Setup...
-cscript.exe /B wget.vbs "http://www.jrsoftware.org/download.php/is-unicode.exe" "%cd%\innosetup-5.5.9-unicode.exe"
+cscript.exe /NoLogo wget.vbs "http://www.jrsoftware.org/download.php/is-unicode.exe" "%cd%\innosetup-5.5.9-unicode.exe"
 echo Installing InnoSetup...
 innosetup-5.5.9-unicode.exe /SILENT
 
 echo Downloading Python 2.7.11...
-cscript.exe /B wget.vbs "https://www.python.org/ftp/python/2.7.11/python-2.7.11.msi" "%cd%\python-2.7.11.msi"
+cscript.exe /NoLogo wget.vbs "https://www.python.org/ftp/python/2.7.11/python-2.7.11.msi" "%cd%\python-2.7.11.msi"
 echo Installing Python 2.7.11...
 python-2.7.11.msi /PASSIVE
 
 echo Downloading Visual C++ For Python27...
-cscript.exe /B wget.vbs "https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi" "%cd%\VCForPython27.msi"
+cscript.exe /NoLogo wget.vbs "https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi" "%cd%\VCForPython27.msi"
 echo Installing Visual C++ For Python27...
 VCForPython27.msi /PASSIVE
 
@@ -38,7 +38,7 @@ C:\Python27\python -m pip install --upgrade pip
 C:\Python27\python -m pip install --upgrade wheel
 
 echo Downloading eSpeak...
-cscript.exe /B wget.vbs "http://pilotfiber.dl.sourceforge.net/project/espeak/espeak/espeak-1.48/setup_espeak-1.48.04.exe" "%cd%\setup_espeak-1.48.04.exe"
+cscript.exe /NoLogo wget.vbs "http://internode.dl.sourceforge.net/project/espeak/espeak/espeak-1.48/setup_espeak-1.48.04.exe" "%cd%\setup_espeak-1.48.04.exe"
 
 echo Downloading FFmpeg...
-cscript.exe /B wget.vbs "https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.7z" "%cd%\ffmpeg-latest-win32-static.7z"
+cscript.exe /NoLogo wget.vbs "https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.7z" "%cd%\ffmpeg-latest-win32-static.7z"
--- MS_Windows_Installer/wget.vbs
+++ MS_Windows_Installer/wget.vbs
@@ -1,11 +1,29 @@
-dim xHttp: Set xHttp = CreateObject("MSXML2.ServerXMLHTTP")
-dim bStrm: Set bStrm = createobject("Adodb.Stream")
-xHttp.Open "GET", WScript.Arguments(0), False
-xHttp.Send
+Set fso = CreateObject ("Scripting.FileSystemObject")
+Set stdout = fso.GetStandardStream (1)
+Set stderr = fso.GetStandardStream (2)
 
-with bStrm
-    .type = 1 '//binary
-    .open
-    .write xHttp.responseBody
-    .savetofile WScript.Arguments(1), 2 '//overwrite
-end with
+if WScript.Arguments.Count < 1 then
+  stdout.WriteLine "Usage: wget.vbs <url> <file>"
+  WScript.Quit
+end if
+
+Set xHttp = CreateObject("WinHttp.WinHttpRequest.5.1")
+xHttp.Open "GET", WScript.Arguments(0), False
+xHttp.Option(6) = true 'The value of false means do not follow redirects automatically
+xHttp.Send()
+
+If xHttp.Status = 200 Then
+  stdout.WriteLine "Saving: " & WScript.Arguments(1)
+  Set bStrm = CreateObject("Adodb.Stream")
+  bStrm.Type = 1 '//binary
+  bStrm.Open
+  bStrm.Write xHttp.responseBody
+  bStrm.SaveToFile WScript.Arguments(1), 2 '//overwrite
+  bStrm.Close
+  Set bStrm = Nothing
+Else
+  stderr.WriteLine "Error Occurred : " & xHttp.Status & " - " & xHttp.statusText
+End If
+
+Set xHttp = Nothing
+WScript.Quit
