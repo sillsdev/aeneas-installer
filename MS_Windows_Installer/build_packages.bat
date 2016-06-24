b6aa1e144229389c361832d5714a5733d34e8fa8
Daniel Bair <daniel@danielbair.com>
2016-06-24 12:51:19 +0700

    Update build_packages.bat to work from fresh clone

--- MS_Windows_Installer/build_packages.bat
+++ MS_Windows_Installer/build_packages.bat
@@ -1,5 +1,24 @@
 @echo off
 
+echo %PATH% >> C:\Windows\Temp\PATH.bak
+IF EXIST "C:\Program Files (x86)" GOTO WIN64PATH
+:WIN32PATH
+  set PATH=C:\Python27\;C:\Python27\Scripts\;%PATH%;C:\Program Files\7-Zip\;C:\Program Files\Inno Setup 5\
+  (call )
+  GOTO SETPATH
+:WIN64PATH
+  set PATH=C:\Python27\;C:\Python27\Scripts\;%PATH%;C:\Program Files (x86)\7-Zip\;C:\Program Files (x86)\Inno Setup 5\
+  (call )
+:SETPATH
+C:\Windows\System32\setx PATH "%PATH%"
+
+C:\Python27\python -m pip install --upgrade pip
+C:\Python27\python -m pip install --upgrade wheel
+C:\Python27\python -m pip install beautifulsoup4
+C:\Python27\python -m pip install lxml
+C:\Python27\python -m pip install numpy
+C:\Python27\python -m pip install aeneas
+
 C:\Python27\python -m pip download pip
 C:\Python27\python -m pip download beautifulsoup4
 C:\Python27\python -m pip download lxml
@@ -22,10 +41,27 @@ python setup.py bdist_wheel
 copy dist\aeneas-1.5.0.3-cp27-cp27m-win32.whl ..\
 cd ..
 
+IF NOT EXIST "%cd%\setup_espeak-1.48.04.exe" (
+  echo Downloading eSpeak...
+  cscript.exe /NoLogo wget.vbs "http://internode.dl.sourceforge.net/project/espeak/espeak/espeak-1.48/setup_espeak-1.48.04.exe" "%cd%\setup_espeak-1.48.04.exe"
+)
+IF NOT EXIST "%cd%\setup_espeak-1.48.04.exe" START https://sourceforge.net/projects/espeak/files/espeak/
+
+IF NOT EXIST "%cd%\ffmpeg-latest-win32-static.7z" (
+  echo Downloading FFmpeg...
+  cscript.exe /NoLogo wget.vbs "https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.7z" "%cd%\ffmpeg-latest-win32-static.7z"
+)
+IF NOT EXIST "%cd%\ffmpeg-latest-win32-static.7z" START https://ffmpeg.zeranoe.com/builds/win32/static/
+
 7z.exe x ffmpeg-*-win32-static.7z -aoa
 move /y ffmpeg-*-win32-static ffmpeg-3.0.2
+
+IF NOT EXIST "%cd%\python-2.7.11.msi" (
+  echo Downloading Python 2.7.11...
+  cscript.exe /NoLogo wget.vbs "https://www.python.org/ftp/python/2.7.11/python-2.7.11.msi" "%cd%\python-2.7.11.msi"
+)
+IF NOT EXIST "%cd%\python-2.7.11.msi" START "https://www.python.org/downloads/release/python-2711/"
 
 ISCC.exe FFmpeg_Installer.iss
 
 ISCC.exe Aeneas_Installer.iss
-
