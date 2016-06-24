174d9f7394842d2f3ef02c3f34278461d92c9b90
Daniel Bair <daniel@danielbair.com>
2016-06-24 12:52:50 +0700

    Update build_packages.bat to work from fresh clone

--- MS_Windows_Installer/build_packages.bat
+++ MS_Windows_Installer/build_packages.bat
@@ -45,13 +45,11 @@ IF NOT EXIST "%cd%\setup_espeak-1.48.04.exe" (
   echo Downloading eSpeak...
   cscript.exe /NoLogo wget.vbs "http://internode.dl.sourceforge.net/project/espeak/espeak/espeak-1.48/setup_espeak-1.48.04.exe" "%cd%\setup_espeak-1.48.04.exe"
 )
-IF NOT EXIST "%cd%\setup_espeak-1.48.04.exe" START https://sourceforge.net/projects/espeak/files/espeak/
 
 IF NOT EXIST "%cd%\ffmpeg-latest-win32-static.7z" (
   echo Downloading FFmpeg...
   cscript.exe /NoLogo wget.vbs "https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.7z" "%cd%\ffmpeg-latest-win32-static.7z"
 )
-IF NOT EXIST "%cd%\ffmpeg-latest-win32-static.7z" START https://ffmpeg.zeranoe.com/builds/win32/static/
 
 7z.exe x ffmpeg-*-win32-static.7z -aoa
 move /y ffmpeg-*-win32-static ffmpeg-3.0.2
@@ -60,7 +58,6 @@ IF NOT EXIST "%cd%\python-2.7.11.msi" (
   echo Downloading Python 2.7.11...
   cscript.exe /NoLogo wget.vbs "https://www.python.org/ftp/python/2.7.11/python-2.7.11.msi" "%cd%\python-2.7.11.msi"
 )
-IF NOT EXIST "%cd%\python-2.7.11.msi" START "https://www.python.org/downloads/release/python-2711/"
 
 ISCC.exe FFmpeg_Installer.iss
 
