ba930d49360d43965c0b0bcb5ae84891191173b8
Daniel Bair <bair.daniel@gmail.com>
2016-06-29 01:29:43 +0700

    Update package name info

--- MS_Windows_Installer/install_packages.bat
+++ MS_Windows_Installer/install_packages.bat
@@ -1,5 +1,5 @@
 @echo off
-
+C:\Python27\python -m ensurepip
 C:\Python27\python -m pip install pip-8.1.2-py2.py3-none-any.whl
 C:\Python27\python -m pip install beautifulsoup4-4.4.1-py2-none-any.whl
 C:\Python27\python -m pip install lxml-3.6.0-cp27-none-win32.whl
