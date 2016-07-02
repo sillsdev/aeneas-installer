955f3e4ed7ea3d6517f6e4863d235c0edc32c96c
Daniel Bair <bair.daniel@gmail.com>
2016-07-03 02:21:33 +0700

    Updated python installer script to latest pip and setuptools

--- Mac_OSX_Installer/install_python.sh
+++ Mac_OSX_Installer/install_python.sh
@@ -6,6 +6,9 @@ rm -f /Users/Shared/python-2.7.11-macosx10.6.pkg.zip
 sudo installer -target / -pkg "$TMPDIR"/python-2.7.11-macosx10.6.pkg
 export PATH=/Library/Frameworks/Python.framework/Versions/2.7/bin:$PATH
 
+pip install -U pip
+pip install -U setuptools
+
 if [ ! -n "$(grep 'export PATH=/Library/Frameworks/Python.framework/Versions/2.7/bin:$PATH' ~/.bash_profile)" ]; then
         touch $HOME/.bash_profile
         echo 'export PATH=/Library/Frameworks/Python.framework/Versions/2.7/bin:$PATH' >> $HOME/.bash_profile
