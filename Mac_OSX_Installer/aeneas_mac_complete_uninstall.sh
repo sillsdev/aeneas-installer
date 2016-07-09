66d37b393d7a790d995376a8eb6ce9cd078083ba
Daniel Bair <daniel@danielbair.com>
2016-07-10 01:39:44 +0700

    Update build scripts for ffmpeg 3.0.2

--- Mac_OSX_Installer/aeneas_mac_complete_uninstall.sh
+++ Mac_OSX_Installer/aeneas_mac_complete_uninstall.sh
@@ -8,7 +8,7 @@ export PATH=/usr/local/bin:/usr/local/sbin:~/bin:$PATH
 
 echo "Uninstalling aeneas 1.5.0.3 and all its dependencies..."
 
-for uninstaller in uninstall-*.sh; do
+for uninstaller in uninstall_*.sh; do
 	bash $uninstaller
 done
 
