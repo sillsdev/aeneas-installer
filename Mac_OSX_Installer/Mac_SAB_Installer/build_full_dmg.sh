8c210f30c4584a4f6ee2b4092a46e2394d3aa137
Daniel Bair <bair.daniel@gmail.com>
2016-07-25 21:44:07 +0700

    Update build dmg for aeneas v1.5.1.0

--- Mac_OSX_Installer/Mac_SAB_Installer/build_full_dmg.sh
+++ Mac_OSX_Installer/Mac_SAB_Installer/build_full_dmg.sh
@@ -10,6 +10,8 @@ brew install danielbair/tap/create-dmg
 
 mkdir -p aeneas-mac-uninstall-scripts
 cp ../_*.sh aeneas-mac-uninstall-scripts
+# VERSION="1.5.0.3"
+VERSION="1.5.1.0"
 DMGFILE="Scripture_App_Builder-1.11.1.dmg"
 BUILDTMP="$(mktemp -d -t createdmg.tmp.XXXXXXXX)"
 cp -r /Applications/Scripture\ App\ Builder.app $BUILDTMP
@@ -21,10 +23,10 @@ create-dmg --volname Scripture_App_Builder \
 	--background background.png \
 	--icon 'Scripture App Builder.app' 142 344 \
 	--app-drop-link 398 344 \
-	--add-file aeneas-mac-setup-1.5.0.3.pkg ../aeneas-mac-setup-1.5.0.3.pkg 654 344 \
+	--add-file aeneas-mac-setup-$VERSION.pkg ../aeneas-mac-setup-$VERSION.pkg 654 344 \
 	--add-folder aeneas-mac-uninstall-scripts aeneas-mac-uninstall-scripts 398 544 \
 	$DMGFILE $BUILDTMP
- rm -rf aeneas-mac-uninstall-scripts
+rm -rf aeneas-mac-uninstall-scripts
 rm -rf "$BUILDTMP"
 open -R "$DMGFILE"
 open "$DMGFILE"
