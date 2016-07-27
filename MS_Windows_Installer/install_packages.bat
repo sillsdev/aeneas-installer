0a64421c4476e36387986fc44a526d8b9dcdd48e
Daniel Bair <bair.daniel@gmail.com>
2016-07-28 00:56:34 +0700

    Update Windows build scripts

--- MS_Windows_Installer/install_packages.bat
+++ MS_Windows_Installer/install_packages.bat
@@ -1,5 +1,28 @@
 @echo off
 
+:--------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    call "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
 IF EXIST "C:\Program Files (x86)" GOTO WIN64PATH
 :WIN32PATH
   set PF32=C:\Program Files
@@ -16,9 +39,10 @@ echo Copying espeak.lib to C:\Python\libs\
 copy /b/v/y espeak.lib C:\Python27\libs\
 
 C:\Python27\python -m ensurepip
-C:\Python27\python -m pip install pip-8.1.2-py2.py3-none-any.whl
-C:\Python27\python -m pip install beautifulsoup4-4.4.1-py2-none-any.whl
-C:\Python27\python -m pip install lxml-3.6.0-cp27-none-win32.whl
-C:\Python27\python -m pip install numpy-1.10.1-cp27-cp27m-win32.whl
+C:\Python27\python -m pip install -U pip-8.1.2-py2.py3-none-any.whl
+C:\Python27\python -m pip install -U beautifulsoup4-4.4.1-py2-none-any.whl
+C:\Python27\python -m pip install -U lxml-3.6.0-cp27-none-win32.whl
+C:\Python27\python -m pip install -U numpy-1.10.1-cp27-cp27m-win32.whl
+C:\Python27\python -m pip uninstall -y aeneas
 C:\Python27\python -m pip install aeneas-1.5.1.0-cp27-cp27m-win32.whl
 
