914a3609284324c9d231ef640db8e9bf630be7ed
Daniel Bair <bair.daniel@gmail.com>
2016-12-07 08:09:41 -0500

    Updates...

--- Mac_OSX_Installer/build_setup.sh
+++ Mac_OSX_Installer/build_setup.sh
@@ -1,33 +1,16 @@
 #!/bin/bash
 
-export PATH=/usr/libexec/git-core/:$PATH
-export PATH=/usr/local/bin:/usr/local/sbin:$PATH
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/libexec/git-core/:$PATH
 
 CURDIR=`dirname $0`
 cd $CURDIR
 
-#HOMEBREW_BUILD_FROM_SOURCE=true
-
-echo Attempting to install Xcode Command Line Tools
-pkgutil --pkgs | grep com.apple | grep CL | grep Tools >/dev/null 2>&1 || ./install_xcode_cl_tools.sh
-xcodebuild -license 2>/dev/null
-
-touch $HOME/.bash_profile
-if [ ! -n "$(grep 'PATH=/usr/local/bin:/usr/local/sbin:$PATH' $HOME/.bash_profile)" ]; then
-	touch $HOME/.bash_profile
-	echo 'export PATH=/usr/local/bin:/usr/local/sbin:$PATH' >> $HOME/.bash_profile
-	chown $USER $HOME/.bash_profile
-fi
-
-echo Installing homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+./install_homebrew.sh
 brew tap danielbair/tap
 echo Running brew update
 brew update
 brew install danielbair/tap/brew-pkg
 brew install gettext
-brew install ruby
-brew link ruby
 
 mkdir -p $HOME/Library/Python/2.7/lib/python/site-packages
 touch $HOME/Library/Python/2.7/lib/python/site-packages/homebrew.pth
