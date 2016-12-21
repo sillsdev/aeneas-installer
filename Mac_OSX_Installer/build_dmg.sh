0522d43c84f8aab8e476d3fe4b61f29d84c02f0a
Daniel Bair <danielbair@Daniels-Virtual-OS-X-Lion.local>
2016-12-21 16:29:08 -0500

    Update to v1.7.1

--- Mac_OSX_Installer/build_dmg.sh
+++ Mac_OSX_Installer/build_dmg.sh
@@ -9,7 +9,7 @@ brew install danielbair/tap/create-dmg
 
 mkdir -p aeneas-mac-uninstall-scripts
 cp _*.sh aeneas-mac-uninstall-scripts
-VERSION="1.7.0.0"
+VERSION="1.7.1"
 DMGFILE="Aeneas_Tools-$VERSION.dmg"
 BUILDTMP="$(mktemp -d -t createdmg.tmp.XXXXXXXX)"
 rm -f "$DMGFILE"
