ec9ae0c65b9d5d7f085c1bfc0ecff2416ddc3f9b
Daniel Bair <daniel@danielbair.com>
2018-01-28 14:28:45 +0700

    Update install_bs4.sh
    RE: https://github.com/Homebrew/brew/issues/3228

--- Mac_OSX_Installer/install_bs4.sh
+++ Mac_OSX_Installer/install_bs4.sh
@@ -14,4 +14,4 @@ if [ ! -n "$(grep 'PATH=/usr/local/bin:/usr/local/sbin:$PATH' $HOME/.bash_profil
 	chown $USER $HOME/.bash_profile
 fi
 
-chown -R $USER:admin /usr/local
+chown -R $USER:admin /usr/local/*
