4a418a143a84403d0fcd7e70b333007c1e79cfd6
Daniel Bair <bair.daniel@gmail.com>
2019-09-05 09:42:53 +0700

    Update build_dmg.sh

--- Mac_OSX_Installer/build_dmg.sh
+++ Mac_OSX_Installer/build_dmg.sh
@@ -18,8 +18,8 @@ create-dmg --volname Aeneas_Tools \
 	--window-pos 20 20 \
 	--icon-size 80 \
 	--add-file aeneas-mac-setup-$VERSION.pkg aeneas-mac-setup-$VERSION.pkg 244 4 \
-	--add-folder aeneas-mac-uninstall-scripts aeneas-mac-uninstall-scripts 344 184 \
-	--add-folder aeneas-mac-installer-packages aeneas-mac-installer-packages 144 184 \
+	--add-folder aeneas-mac-uninstall-scripts aeneas-mac-uninstall-scripts 344 144 \
+	--add-folder aeneas-mac-installer-packages aeneas-mac-installer-packages 144 144 \
 	$DMGFILE $BUILDTMP
 rm -rf "$BUILDTMP"
 open -R "$DMGFILE"
