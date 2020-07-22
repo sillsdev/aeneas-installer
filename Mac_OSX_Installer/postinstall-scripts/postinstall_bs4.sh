9d58b2dc4bcd4100eac4a237f7536849b37a3374
Daniel Bair <daniel@danielbair.com>
2020-07-22 08:07:50 +0700

    Updated postinstall scripts

--- Mac_OSX_Installer/postinstall-scripts/postinstall_bs4.sh
+++ Mac_OSX_Installer/postinstall-scripts/postinstall_bs4.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export PATH=/opt/usr/bin:/opt/usr/sbin:$PATH
+export PATH="/opt/usr/bin:/opt/usr/sbin:$PATH"
 
 touch $HOME/.bash_profile
-if [ ! -n "$(grep '^export PATH=/opt/usr/bin:/opt/usr/sbin:' $HOME/.bash_profile)" ]; then
-	echo 'export PATH=/opt/usr/bin:/opt/usr/sbin:$PATH' >> $HOME/.bash_profile
+if [ ! -n "$(grep '^export PATH"=/opt/usr/bin:/opt/usr/sbin:' $HOME/.bash_profile)" ]; then
+	echo 'export PATH="/opt/usr/bin:/opt/usr/sbin:$PATH"' >> $HOME/.bash_profile
 	chown $USER $HOME/.bash_profile
 fi
 
