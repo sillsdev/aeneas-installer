3ecf19365c3e4e4ae230c2721d38fa7174a89dce
Daniel Bair <bair.daniel@gmail.com>
2020-07-29 19:01:58 +0700

    Updated new build product names

--- MS_Windows_Installer/sign_installer.sh
+++ MS_Windows_Installer/sign_installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-osslsigncode sign -pkcs12 "$HOME/MacInstaller.p12" -askpass -n "Aeneas Tools" -i "http://www.readbeyond.it/aeneas/" -t "http://timestamp.comodoca.com/authenticode" -in "./aeneas-windows-setup-1.7.3.0_3.exe" -out "./aeneas-windows-setup-1.7.3.0_3_signed.exe"
-osslsigncode verify "./aeneas-windows-setup-1.7.3.0_3_signed.exe"
+osslsigncode sign -pkcs12 "$HOME/MacInstaller.p12" -askpass -n "Aeneas Tools" -i "http://www.readbeyond.it/aeneas/" -t "http://timestamp.comodoca.com/authenticode" -in "./aeneas-win32-setup-1.7.3.0_3.exe" -out "./aeneas-win32-setup-1.7.3.0_3_signed.exe"
+osslsigncode verify "./aeneas-win32-setup-1.7.3.0_3_signed.exe"
 
